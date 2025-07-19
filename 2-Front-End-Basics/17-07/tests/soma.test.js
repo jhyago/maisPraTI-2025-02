2dfef0a55eeda7af344f48bd688d7765b610654a
jhyago <jaquesantunes@protonmail.com>
2025-07-19 12:22:28 -0300

    Atualizando o repositório da Turma 02

--- 2-Front-End-Basics/17-07/tests/soma.test.js
+++
@@ -1,9 +0,0 @@
-const soma = require('../src/soma')
-
-test('Soma 2 + 3 deve ser igual a 5', () => {
-    expect(soma(2, 3)).toBe(5)
-})
-
-test('soma -1 e +1 deve ser igual a 0', () => {
-    expect(soma(-1, 1)).toBe(0)
-})
