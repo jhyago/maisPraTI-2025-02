c068e1cb30dec9efd6b6ab73ccc210d31d983809
MauricioTdM <mauricio1tmelo@yahoo.com.br>
2025-08-04 21:14:04 -0300

    feat: consumindo API

--- 3-Reactjs/3-components/src/main.jsx
+++ 3-Reactjs/3-components/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import LifeCycleFunctionalComponent from './components/LifeCycleFunctionalComponent'
-import FunctionalComponentViaCep from './components/FunctionalComponentViaCep'
+// import FunctionalComponentViaCep from './components/FunctionalComponentViaCep'
+import ViaCepFunctional from './components/ViaCepFunctional'
 
 // Desenvolva um componente React (versão classe e versão funcional) que consome a API viacep para buscar e exibir o endereço correspondente a um CEP recebido por prop, de modo a evidenciar as fases de montagem, atualização e desmontagem.
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FunctionalComponentViaCep />
+    <ViaCepFunctional cep={"21021370"} />
   </StrictMode>,
 )
