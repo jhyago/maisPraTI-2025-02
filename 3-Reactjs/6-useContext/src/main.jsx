66a29fd4356a4464999da6dc3136e27303e60eae
jhyago <jaquesantunes@protonmail.com>
2025-08-18 08:57:35 -0300

    Desafio da aula sobre Contextos

--- 3-Reactjs/6-useContext/src/main.jsx
+++ 3-Reactjs/6-useContext/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { AuthProvider } from './context/auth.jsx'
+import App from './App'
+import { AuthProvider } from './context/auth'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <App />
-    </AuthProvider>    
-  </StrictMode>
+    </AuthProvider>
+  </StrictMode>,
 )
