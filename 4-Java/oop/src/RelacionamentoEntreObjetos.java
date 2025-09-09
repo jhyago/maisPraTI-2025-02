95097c4a998e1b77db798566bac6e61ec795ae42
jhyago <jaquesantunes@protonmail.com>
2025-09-09 19:46:18 -0300

    Desafio de Collections

--- 4-Java/oop/src/RelacionamentoEntreObjetos.java
+++ 4-Java/oop/src/RelacionamentoEntreObjetos.java
@@ -91,12 +91,12 @@ public class RelacionamentoEntreObjetos {
     }
 }
 
-//Implemente um sistema de gerenciamento de veículos e seus proprietários. O sistema deve utilizar os conceitos de associação, agregação, composição, herança, polimorfismo e dependência.
-//
+//Implemente um sistema de gerenciamento de veículos e seus proprietários. O sistema deve utilizar os conceitos de associação, agregação, composição, herança e polimorfismo.
+
 //O sistema deve ter uma classe abstrata chamada Veiculo, com atributos básicos como marca, modelo e ano, e um método para exibir essas informações. Além disso, o método tipoDeVeiculo() deverá ser implementado nas subclasses Carro e Moto, que devem herdar de Veiculo. Cada tipo de veículo deve exibir seu tipo específico.
-//
+
 //Crie uma classe Proprietario, que possui informações como nome e endereço, e que pode ter múltiplos veículos associados a ele. A relação entre proprietário e veículos deve ser uma associação.
-//
+
 //Implemente uma classe Servico, que tem informações sobre a descrição e o preço dos serviços realizados, e uma classe Oficina, que é responsável por adicionar e listar serviços realizados.
-//
-//Entre as classes, estabeleça as relações necessárias, como agregação entre oficina e serviços e dependência entre proprietário e veículos. Utilize herança e polimorfismo para organizar e expandir o sistema de forma eficiente.
+
+//Entre as classes, estabeleça as relações necessárias, como agregação entre oficina e serviços e associação entre proprietário e veículos. Utilize herança e polimorfismo para organizar e expandir o sistema de forma eficiente.
