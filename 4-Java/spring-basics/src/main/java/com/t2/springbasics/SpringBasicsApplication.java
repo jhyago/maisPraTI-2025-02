acbb07c9a367195f5063e7d143ee14778ba7d095
jhyago <jaquesantunes@protonmail.com>
2025-09-22 21:41:23 -0300

    IoC Training

---
+++ 4-Java/spring-basics/src/main/java/com/t2/springbasics/SpringBasicsApplication.java
@@ -0,0 +1,23 @@
+package com.t2.springbasics;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicsApplication.class, args);
+    }
+//    Construir uma mini-aplicação que imprime o sabor do sorvete servido por uma máquina.
+//    Você fará Parte A (sem IoC) e Parte B (com IoC/Spring), trocando a implementação no meio para ver o impacto.
+
+    @Bean
+    CommandLineRunner init(Carro carro) {
+        return args -> {
+            System.out.println("" + carro.tipo());
+        };
+    }
+}
