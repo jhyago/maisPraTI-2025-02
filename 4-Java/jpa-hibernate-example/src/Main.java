9a3606ad217e69eb92bae6dc2395686faa3673d4
jhyago <jaquesantunes@protonmail.com>
2025-10-11 12:15:19 -0300

    Test problem

--- 4-Java/jpa-hibernate-example/src/Main.java
+++ 4-Java/jpa-hibernate-example/src/Main.java
@@ -5,7 +5,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-
 public class Main {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
